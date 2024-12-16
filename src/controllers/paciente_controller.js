25c7a95a7dd85be8d88c7d3c4b1a6a67571ef6b1
Andres <andresftma@gmail.com>
2024-12-15 23:53:59 -0500

    cambiar password y actualizar perfil

--- src/controllers/paciente_controller.js
+++ src/controllers/paciente_controller.js
@@ -77,13 +77,94 @@ const recuperarPassword = async (req, res) => {
     await pacienteBDD.save();
     res.status(200).json({ msg: "Revisa tu correo para restablecer tu cuenta" });
 }
+const comprobarTokenPassword = async (req,res) => {
+
+    // Paso 1: Tomar los datos del request (correo electrónico y contraseña)
+    const {token}=req.params
+    // Paso 2: Validar datos
+    if(!(token))return res.status(400).json({msg:"Lo sentimos no se puede validar la cuenta"})
+    const pacienteBDD = await Paciente.findOne({token})
+    if (pacienteBDD?.token !== token) res.status(404).json({msg:"Lo sentimos no se puede validar la cuenta"})
+
+    // Paso 3: Interactuar con BDD
+    await pacienteBDD.save()
+    res.status(200).json({msg: "Token Confirmado ya puedes crear tu nuevo password"})
+    
+
+}
+const nuevoPassword = async (req,res) => {
+
+    // Paso 1: Tomar los datos del request (correo electrónico y contraseña)
+
+    const{password,confirmpassword} = req.body
+
+    // Paso 2: Validar datos
+
+    if (Object.values(req.body).includes("")) return res.status(404).json({msg:"Lo sentimos, debes llenar todos los campos"})
 
 
+    if(password != confirmpassword) return res.status(404).json({msg:"Lo sentimos, los passwords no coinciden"})
+    const pacienteBDD = await Paciente.findOne({token:req.params.token})
+
+    if(pacienteBDD?.token !== req.params.token) return res.status(404).json({msg:"Lo sentimos, no se puede validar la cuenta"})
+
+    // Paso 3: Interactuar con BDD
+
+    pacienteBDD.token = null
+    pacienteBDD.password = await pacienteBDD.encrypPassword(password)
+    await pacienteBDD.save()
+    res.status(200).json({msg:"Felicitaciones, ya puedes iniciar sesión con tu nuevo password"}) 
+  
+}
+const perfilUsuario = (req,res) => {
+    delete req.pacienteBDD.token
+    delete req.pacienteBDD.confirmEmail
+    delete req.pacienteBDD.createdAt
+    delete req.pacienteBDD.updatedAt
+    delete req.pacienteBDD.__v
+    res.status(200).json(req.veterinarioBDD)
+}
+
+const actualizarPassword = async (req,res)=>{
+    const pacienteBDD = await Veterinario.findById(req.veterinarioBDD._id)
+    if(!pacienteBDD) return res.status(404).json({msg:`Lo sentimos, no existe el veterinario ${id}`})
+    const verificarPassword = await veterinarioBDD.matchPassword(req.body.passwordactual)
+    if(!verificarPassword) return res.status(404).json({msg:"Lo sentimos, el password actual no es el correcto"})
+        pacienteBDD.password = await veterinarioBDD.encrypPassword(req.body.passwordnuevo)
+    await pacienteBDD.save()
+    res.status(200).json({msg:"Password actualizado correctamente"})
+}
+
+const actualizarPerfil = async (req,res)=>{
+    const {id} = req.params
+    if( !mongoose.Types.ObjectId.isValid(id) ) return res.status(404).json({msg:`Lo sentimos, debe ser un id válido`});
+    if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Lo sentimos, debes llenar todos los campos"})
+    const pacienteBDD = await Veterinario.findById(id)
+    if(!pacienteBDD) return res.status(404).json({msg:`Lo sentimos, no existe el veterinario ${id}`})
+    if (pacienteBDD.email !=  req.body.email)
+    {
+        const pacienteBDDMail = await Veterinario.findOne({email:req.body.email})
+        if (pacienteBDDMail)
+        {
+            return res.status(404).json({msg:`Lo sentimos, el existe ya se encuentra registrado`})  
+        }
+    }
+		veterinarioBDD.nombre = req.body.nombre || veterinarioBDD?.nombre
+    veterinarioBDD.apellido = req.body.apellido  || veterinarioBDD?.apellido
+    veterinarioBDD.direccion = req.body.direccion ||  veterinarioBDD?.direccion
+    veterinarioBDD.telefono = req.body.telefono || veterinarioBDD?.telefono
+    veterinarioBDD.email = req.body.email || veterinarioBDD?.email
+    await veterinarioBDD.save()
+    res.status(200).json({msg:"Perfil actualizado correctamente"})
+}
 export {
     registro,
     confirmEmail,
     login,
     recuperarPassword,
-      
- 
+    comprobarTokenPassword,
+    nuevoPassword,
+    perfilUsuario,
+    actualizarPassword,
+    actualizarPerfil
 }
--- src/routers/paciente_routers.js
+++ src/routers/paciente_routers.js
@@ -1,5 +1,6 @@
 import {Router} from 'express'
-import { confirmEmail, login, registro} from '../controllers/paciente_controller.js'
+import { actualizarPassword, actualizarPerfil, comprobarTokenPassword, confirmEmail, login, nuevoPassword, perfilUsuario, recuperarPassword, registro} from '../controllers/paciente_controller.js'
+import { verificarAutenticacion } from '../helpers/crearJWT.js'
 
 
 const router = Router()
@@ -11,6 +12,15 @@ router.get('/confirmar/:token',confirmEmail)
 
 router.post('/login',login)
 
+router.post('/recuperar-password',recuperarPassword)
 
+router.get('/recuperar-password/:token',comprobarTokenPassword)
+
+router.post('/nuevo-password/:token',nuevoPassword)
+
+//Rutas Privadas
+router.get('/perfilpaciente',verificarAutenticacion,perfilUsuario)
+router.put('/veterinario/actualizarpassword',verificarAutenticacion,actualizarPassword)
+router.put('/veterinario/:id',verificarAutenticacion,actualizarPerfil)
 
 export default router
